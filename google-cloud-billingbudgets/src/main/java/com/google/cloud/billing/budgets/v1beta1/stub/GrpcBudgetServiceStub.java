267658441df8fae3f3018d96361ff658c1e1757c
yoshi-automation <yoshi-automation@google.com>
2020-12-10 14:54:25 -0800

    chore: update gapic-generator-java to 0.0.9
    Committer: @miraleung
    PiperOrigin-RevId: 346372957
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Dec 8 11:37:30 2020 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 857f70daef9f4c38f042fb3dfb7b8423ae18fd19
    Source-Link: https://github.com/googleapis/googleapis/commit/857f70daef9f4c38f042fb3dfb7b8423ae18fd19

--- google-cloud-billingbudgets/src/main/java/com/google/cloud/billing/budgets/v1beta1/stub/GrpcBudgetServiceStub.java
+++ google-cloud-billingbudgets/src/main/java/com/google/cloud/billing/budgets/v1beta1/stub/GrpcBudgetServiceStub.java
@@ -119,11 +119,21 @@ public class GrpcBudgetServiceStub extends BudgetServiceStub {
         BudgetServiceStubSettings.newBuilder().build(), clientContext, callableFactory);
   }
 
+  /**
+   * Constructs an instance of GrpcBudgetServiceStub, using the given settings. This is protected so
+   * that it is easy to make a subclass, but otherwise, the static factory methods should be
+   * preferred.
+   */
   protected GrpcBudgetServiceStub(BudgetServiceStubSettings settings, ClientContext clientContext)
       throws IOException {
     this(settings, clientContext, new GrpcBudgetServiceCallableFactory());
   }
 
+  /**
+   * Constructs an instance of GrpcBudgetServiceStub, using the given settings. This is protected so
+   * that it is easy to make a subclass, but otherwise, the static factory methods should be
+   * preferred.
+   */
   protected GrpcBudgetServiceStub(
       BudgetServiceStubSettings settings,
       ClientContext clientContext,
